c0254f8ea7924c8e8cda310b68066d243a362ae7
Anton Wetzel <anton.wetzel@wetteronline.de>
2025-09-26 08:41:12 +0200

    fix script

--- serve.sh
+++ serve.sh
@@ -1,5 +1,7 @@
 npx yarn install
 npx yarn ubrn build web
+# https://github.com/jhugman/uniffi-bindgen-react-native/issues/306
+grep -rl --include='*.ts' 'async public' . | xargs sed -i '' 's/async public/public async/g'
 npx esbuild src/main.ts --bundle --outfile=out/bundle.js --format=esm --target=esnext --platform=browser --loader:.wasm=file
 cp index.html out/index.html
 basic-http-server out
